f36745158dc7fe805f1257b87a34eeedad4694a5
Rafael Schmid <rafaelfschmid@gmail.com>
2016-05-18 11:43:49 -0300

    Removing old elapsed times and adding new.

--- src/scripts/all_equal_10.sh
+++ src/scripts/all_equal_10.sh
@@ -1,3 +1,7 @@
+./scripts/exec.sh fixsort/bitonic/bitonicsort.exe ~/inputs/equal-10 > timesequal-10/bitonicsort.time
+./scripts/exec.sh fixsort/mergesort/mergesort.exe ~/inputs/equal-10 > timesequal-10/mergesort.time
+./scripts/exec.sh fixsort/oddevensort/oddevensort.exe ~/inputs/equal-10 > timesequal-10/oddevensort.time
+./scripts/exec.sh fixsort/quicksort/quicksort.exe ~/inputs/equal-10 > timesequal-10/quicksort.time
 ./scripts/exec.sh mergeseg.exe ~/inputs/inputsequal-10 > timesequal-10/mergeseg.time
 ./scripts/exec.sh radixseg.exe ~/inputs/inputsequal-10 > timesequal-10/radixseg.time
 ./scripts/exec.sh fixcub.exe ~/inputs/inputsequal-10 > timesequal-10/fixcub.time
