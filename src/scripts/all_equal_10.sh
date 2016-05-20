4b53d1f600211254d0943baa8199ece020e68fad
schmid <Rafael Schmid>
2016-05-20 20:34:24 -0300

    Elapsed times

--- src/scripts/all_equal_10.sh
+++ src/scripts/all_equal_10.sh
@@ -4,10 +4,10 @@ out=$2
 ./scripts/exec.sh mergeseg.exe $in > $out/mergeseg.time
 ./scripts/exec.sh radixseg.exe $in > $out/radixseg.time
 ./scripts/exec.sh fixcub.exe $in > $out/fixcub.time
-./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
 ./scripts/exec.sh fixsort/bitonic/bitonicsort.exe $in > $out/bitonicsort.time
 ./scripts/exec.sh fixsort/mergesort/mergesort.exe $in > $out/mergesort.time
 ./scripts/exec.sh fixsort/oddevensort/oddevensort.exe $in > $out/oddevensort.time
 ./scripts/exec.sh fixsort/quicksort/quicksort.exe $in > $out/quicksort.time
+./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
 
 
