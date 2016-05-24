afd1094824cbecb9fbf14b134412d0dd9190c6c5
Rafael Schmid <rafaelfschmid@gmail.com>
2016-05-24 16:59:06 -0300

    tempos gtxtitan

--- src/scripts/all_equal_10.sh
+++ src/scripts/all_equal_10.sh
@@ -1,14 +1,14 @@
 in=$1
 out=$2
 
+./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
 ./scripts/exec.sh mergeseg.exe $in > $out/mergeseg.time
 ./scripts/exec.sh radixseg.exe $in > $out/radixseg.time
 ./scripts/exec.sh fixcub.exe $in > $out/fixcub.time
 ./scripts/exec.sh fixthrust.exe $in > $out/fixthrust.time
-#./scripts/exec.sh fixsort/bitonic/bitonicsort.exe $in > $out/fixbitonic.time
-#./scripts/exec.sh fixsort/mergesort/mergesort.exe $in > $out/fixmerge.time
-#./scripts/exec.sh fixsort/oddevensort/oddevensort.exe $in > $out/fixoddeven.time
-#./scripts/exec.sh fixsort/quicksort/quicksort.exe $in > $out/fixquick.time
-./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
+./scripts/exec.sh fixsort/bitonic/bitonicsort.exe $in > $out/fixbitonic.time
+./scripts/exec.sh fixsort/mergesort/mergesort.exe $in > $out/fixmerge.time
+./scripts/exec.sh fixsort/oddevensort/oddevensort.exe $in > $out/fixoddeven.time
+./scripts/exec.sh fixsort/quicksort/quicksort.exe $in > $out/fixquick.time
 
 
