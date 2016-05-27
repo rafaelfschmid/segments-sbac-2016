7bea4a826a67c78d58e8f6f384c9dff30623b739
Rafael Schmid <rafaelfschmid@gmail.com>
2016-05-27 09:18:59 -0300

    bitonic mergemgpu

---
+++ src/fixmergemgpu.cu
@@ -0,0 +1,140 @@
+/*
+ * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+#include <moderngpu/kernel_mergesort.hxx>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <utility>
+#include <iostream>
+#include <bitset>
+#include <math.h>
+#include <time.h>
+#include <chrono>
+#include <cuda.h>
+//#include <cstdlib>
+#include <iostream>
+#include <chrono>
+
+#ifndef ELAPSED_TIME
+#define ELAPSED_TIME 0
+#endif
+
+void cudaTest(cudaError_t error) {
+	if (error != cudaSuccess) {
+		printf("cuda returned error %s (code %d), line(%d)\n",
+				cudaGetErrorString(error), error, __LINE__);
+		exit (EXIT_FAILURE);
+	}
+}
+
+void print(uint* host_data, uint n) {
+	std::cout << "\n";
+	for (uint i = 0; i < n; i++) {
+		std::cout << host_data[i] << " ";
+	}
+	std::cout << "\n";
+}
+////////////////////////////////////////////////////////////////////////////////
+// Test driver
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv) {
+	uint num_of_segments;
+	uint num_of_elements;
+	uint i;
+
+	scanf("%d", &num_of_segments);
+	uint mem_size_seg = sizeof(int) * (num_of_segments + 1);
+	uint *h_seg = (uint *) malloc(mem_size_seg);
+	for (i = 0; i < num_of_segments + 1; i++)
+		scanf("%d", &h_seg[i]);
+
+	scanf("%d", &num_of_elements);
+	uint mem_size_vec = sizeof(int) * num_of_elements;
+	uint *h_vec = (uint *) malloc(mem_size_vec);
+	for (i = 0; i < num_of_elements; i++) {
+		scanf("%d", &h_vec[i]);
+	}
+
+	uint *h_norm = (uint *) malloc(mem_size_seg);
+	uint previousMax = 0;
+	for (i = 0; i < num_of_segments; i++) {
+		uint currentMin = h_vec[h_seg[i]];
+		uint currentMax = h_vec[h_seg[i]];
+
+		for (uint j = h_seg[i] + 1; j < h_seg[i + 1]; j++) {
+			if (h_vec[j] < currentMin)
+				currentMin = h_vec[j];
+			else if (h_vec[j] > currentMax)
+				currentMax = h_vec[j];
+		}
+
+		uint normalize = previousMax - currentMin;
+		h_norm[i] = ++normalize;
+		for (uint j = h_seg[i]; j < h_seg[i + 1]; j++) {
+			h_vec[j] += normalize;
+		}
+		previousMax = currentMax + normalize;
+	}
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	uint *d_vec;
+
+	cudaTest(cudaMalloc((void **) &d_vec, mem_size_vec));
+	cudaTest(cudaMalloc((void **) &d_value, mem_size_vec));
+
+	for (int i = 0; i < EXECUTIONS; i++) {
+
+		cudaTest(cudaMemcpy(d_vec, h_vec, mem_size_vec, cudaMemcpyHostToDevice));
+
+		cudaEventRecord(start);
+		mgpu::standard_context_t context;
+		mgpu::mergesort(d_vec, num_of_elements, mgpu::less_t<int>(), context);
+		cudaEventRecord(stop);
+
+		cudaError_t errSync = cudaGetLastError();
+		cudaError_t errAsync = cudaDeviceSynchronize();
+		if (errSync != cudaSuccess)
+			printf("Sync kernel error: %s\n", cudaGetErrorString(errSync));
+		if (errAsync != cudaSuccess)
+			printf("Async kernel error: %s\n", cudaGetErrorString(errAsync));
+
+		if (ELAPSED_TIME == 1) {
+			cudaEventSynchronize(stop);
+			float milliseconds = 0;
+			cudaEventElapsedTime(&milliseconds, start, stop);
+			std::cout << milliseconds << "\n";
+		}
+
+		cudaDeviceSynchronize();
+	}
+
+	cudaMemcpy(h_vec, d_vec, mem_size_vec, cudaMemcpyDeviceToHost);
+
+	for (i = 0; i < num_of_segments; i++) {
+		for (int j = h_seg[i]; j < h_seg[i + 1]; j++) {
+			h_vec[j] -= h_norm[i];
+		}
+	}
+
+	cudaFree(d_vec);
+
+	if (ELAPSED_TIME != 1) {
+		print(h_vec, num_of_elements);
+	}
+
+	free(h_seg);
+	free(h_vec);
+	free(h_norm);
+}
