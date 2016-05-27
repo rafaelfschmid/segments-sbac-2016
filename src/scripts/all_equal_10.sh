70ae274bab34b4ea66faaa12e0e9dfa4ad10496c
Rafael Schmid <rafaelfschmid@gmail.com>
2016-05-27 12:54:09 -0300

    bitonic e merge

--- src/scripts/all_equal_10.sh
+++ src/scripts/all_equal_10.sh
@@ -1,14 +1,18 @@
 in=$1 # input files dir
 out=$2 # time files dir
 
-./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
-./scripts/exec.sh mergeseg.exe $in > $out/mergeseg.time
-./scripts/exec.sh radixseg.exe $in > $out/radixseg.time
-./scripts/exec.sh fixcub.exe $in > $out/fixcub.time
-./scripts/exec.sh fixthrust.exe $in > $out/fixthrust.time
-./scripts/exec.sh fixsort/bitonic/bitonicsort.exe $in > $out/fixbitonic.time
-./scripts/exec.sh fixsort/mergesort/mergesort.exe $in > $out/fixmerge.time
-./scripts/exec.sh fixsort/oddevensort/oddevensort.exe $in > $out/fixoddeven.time
-./scripts/exec.sh fixsort/quicksort/quicksort.exe $in > $out/fixquick.time
+./scripts/exec.sh fixmergemgpu.exe $in > $out/fixmergemgpu.time
+./scripts/exec.sh fixsort/bitonicseg/bitonicseg.exe $in > $out/bitonicseg.time
+
+
+#./scripts/exec.sh fixpreprocess.exe $in > $out/fixpreprocess.time
+#./scripts/exec.sh mergeseg.exe $in > $out/mergeseg.time
+#./scripts/exec.sh radixseg.exe $in > $out/radixseg.time
+#./scripts/exec.sh fixcub.exe $in > $out/fixcub.time
+#./scripts/exec.sh fixthrust.exe $in > $out/fixthrust.time
+#./scripts/exec.sh fixsort/bitonic/bitonicsort.exe $in > $out/fixbitonic.time
+#./scripts/exec.sh fixsort/mergesort/mergesort.exe $in > $out/fixmerge.time
+#./scripts/exec.sh fixsort/oddevensort/oddevensort.exe $in > $out/fixoddeven.time
+#./scripts/exec.sh fixsort/quicksort/quicksort.exe $in > $out/fixquick.time
 
 
