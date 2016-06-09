a9e73479c5e032af7dd225a87c2b0d2cec79f351
schmid <Rafael Schmid>
2016-06-09 09:22:23 -0300

    times updates

--- src/scripts/parsetimes.sh
+++ src/scripts/parsetimes.sh
@@ -2,6 +2,7 @@ dir=$1
 
 ./parser.exe $dir/fixbitonic.time $dir/00fixbitonic.time
 ./parser.exe $dir/fixpass.time $dir/00fixpass.time
+./parser.exe $dir/fixpassdiff.time $dir/00fixpassdiff.time
 ./parser.exe $dir/fixcub.time $dir/00fixcub.time
 ./parser.exe $dir/fixmerge.time $dir/00fixmerge.time
 ./parser.exe $dir/fixmergemgpu.time $dir/00fixmergemgpu.time
