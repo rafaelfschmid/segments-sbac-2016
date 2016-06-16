5d00d146a4e55a64fa606d70a17b850daa4fc1c7
Rafael Schmid <rafaelfschmid@gmail.com>
2016-06-16 10:57:31 -0300

    times radix 6, 12, 18

--- src/scripts/exec_sorts.sh
+++ src/scripts/exec_sorts.sh
@@ -1,4 +1,5 @@
 
-./scripts/exec_all.sh ~/inputs/equalasc-12 timesequalasc-12
-./scripts/exec_all.sh ~/inputs/equaldesc-12 timesequaldesc-12
+./scripts/exec_all.sh ~/inputs/radix/equal-6 radix/timesequal-6
+./scripts/exec_all.sh ~/inputs/radix/equal-12 radix/timesequal-12
+./scripts/exec_all.sh ~/inputs/radix/equal-18 radix/timesequal-18
 
